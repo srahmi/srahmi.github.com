e52283f29ee2d73b7216564b3cf71936d5a6e61a
Saïd RAHMI <srahmi@mediametrie.fr>
2015-12-24 14:55:17 +0100

    updating master

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name srahmigithubcomApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the srahmigithubcomApp
+ */
+angular.module('srahmigithubcomApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
